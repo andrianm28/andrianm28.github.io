fc94888fb67f75775f10a377ea84a1592af43d5c
Mochammad Andrian Maulana <andrianmaulana28@gmail.com>
2020-10-23 19:30:59 +0700

    release

--- script.js
+++ script.js
@@ -8,36 +8,23 @@ const cpmHTML = document.querySelector(".CPM")
 const timer = document.querySelector(".timer")
 
 let testText = document.querySelector("#text-case")
-let minutes = 0, seconds = 0, ms = 0
-let alertMessage = "Congratulations, you completed the test in "  + minutes + " minutes and " + seconds + "." + ms + " seconds"
-let interval
-let spanArray = []
-let letterArray = []
-let WPM = 0
-let CPM = 0
-let numWords = 0
-let letterIndex = 0
-let wordIndex = 0
-let numberOfMistakes = 0
-let errorState
+let minutes = 0, seconds = 0, ms = 0, WPM = 0, CPM = 0
+let numWords = 0, letterIndex = 0, wordIndex = 0, numberOfMistakes = 0
+let interval, spanArray = [], letterArray = [], errorState
 
 function displayZero(time){
-    if(time <= 9) {
-        time = "0" + time
-    }
+    time <= 9 ? time = "0" + time : time
     return time
 }
 
 function runTimer(){ 
     timer.innerHTML = displayZero(minutes) + ":" + displayZero(seconds) + ":" + displayZero(ms)
     ms++
-
     displayWPM_and_CPM(minutes, seconds, ms);
     if(seconds === 60){
         seconds = 0
         minutes++
     }
-
     if(ms === 100){
         ms = 0
         seconds++
@@ -45,11 +32,8 @@ function runTimer(){
 }
 
 function startTimer(){
-    let textLength = text.value.length;
-
-    if(textLength === 0){
-        interval = setInterval(runTimer, 10)
-    }
+    let textLength = text.value.length
+    textLength === 0 ? interval = setInterval(runTimer, 10) : interval
 }
 
 function restartTest(){
@@ -66,17 +50,15 @@ function restartTest(){
     CPM = 0
     numWords = 0
     numberOfMistakes = 0
-    
     spanArray.map(char => {
-        char.style.background = 'lightblue'
+        char.style.background = 'white'
         char.style.color = 'black'
     });
 }
 
 function countWords(){
     let textLength = text.value.length;
-    if(text.value.substring((textLength-1), textLength) == " ")
-        numWords++
+    text.value.substring((textLength-1), textLength) == " " ? numWords++ : numWords
 }
 
 function displayWPM_and_CPM(mins, secs, ms){
@@ -98,11 +80,10 @@ function checkStringEquality() {
     console.log(textInput)
     console.log(completeTestText)
     console.log(timer.innerHTML)
-
     if(timer.innerHTML[1] === '1'){
         text.style.border = "8px solid green"
         clearInterval(interval)
-        alert((`Your score: `+ wpmHTML.innerHTML + cpmHTML.innerHTML))
+        alert((`Your score: ${wpmHTML.innerHTML} ${cpmHTML.innerHTML}`))
     } else if(textInput == parsedTestText){
         errorState = false
         text.style.border = "8px solid lightblue"
@@ -153,7 +134,6 @@ function createTestText(text){
     spanArray = []
     letterArray = [];
     testText.innerHTML = ""
-
     for(let i = 0; i < separatedText.length; i++){
         let separatedWord = document.createElement("span")
         let node = document.createTextNode((separatedText[i] + " "))
@@ -164,8 +144,8 @@ function createTestText(text){
     createLetterArray()
 }
 
-textcase = `Tos emam teu acan Bade kamana deui Iraha kadieu Tos Lami teu uih`
-createTestText(textcase)
+textcase = `Tos emam teu acan Bade kamana deui Iraha kadieu Tos Lami teu uih Kumaha ieu urang teu bisa Ulah loba gaya Geus teu kuat Hayang Kawin Ulah nyo'o hp wae Hapunten Abdi teu Tiasa dongkap Teu kuat hayang seuri Punten upami WC palih mana nya Neng aa sono Punten pangnyandakeun eta Maneh mah jahat ih Geura nyangu geus lapar Ti isuk can mandi Ulah garelut didieu Abdi teu damang Geura sare geus peuting Loba laleur di imah maneh mah Eta ucing paraban Karunya Bapa abdi bageur Mamah abdi bageur Maneh mah beuki heureuy Ulah lila teuing atuh Maneh mah ngabohong wae Lanceuk urang mah lalaki Tong tarik teuing atuh mawana Hayang nginum Sorangan wae Ti peuting keneh can balik Tos lami pupus Geura dicalana Teu gaduh acis Keur beberesih Maneh haus teu Teu kadenge ulah laun teuing Abdi bogoh kasalira Ulah loba gaya bisi dicabok Eta baju tilepan Ulah kadieu ngebul  Kin antosan sakedap deui Meuli sangu timana Mani keueung peuting tadi Malam tadi begitu sunyi Abdi sieun ku dosa Keur ngorong Maen bola jeung si eta mah di tengkasan wae Ulin atuh tong cicing wae Arateul awak`
+createTestText(textcase.toLowerCase())
 text.addEventListener("keyup", checkStringEquality, false)
 text.addEventListener("keydown", highlightWords, false)
 text.addEventListener("keypress", startTimer, false)
